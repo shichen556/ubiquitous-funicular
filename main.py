7fd347988973d45379abc9cb4f084fab7a6fd594
shichen556 <shichenlinxd@gmail.com>
2025-11-02 23:03:30 +0100

    derefactorization: Description: first the fuctionality, then the refactor

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 import pygame
 from sys import exit
-
-def load_font(x, y, text=None, font_type=None, font_size=50):
-    # Load and render font
-    if text is None and font_type is not None:
-        font = pygame.font.Font(f"assets/font/{font_type}.ttf", font_size)
-        font_surf = font.render(font_type, True, "Black")    
-
-    else:
-        font = pygame.font.Font(f"assets/font/{font_type}.ttf", font_size)
-        font_surf = font.render(text, True, "Black")
-        
-    font_rect = font_surf.get_rect(center = (x, y))
-    
-    return (font_surf, font_rect)
-
-def display_text(x, y, text=None, font_type=None, font_size=50):
-    # load and render font
-    font = load_font(x, y, text, font_type, font_size)
-    
-    # display font
-    screen.blit(font[0], font[1]) # surf, rect
     
 # Initialize pygame
 pygame.init()
@@ -36,7 +15,31 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
 # Menu buttons setup
+central_pos_x = WIDTH / 2
 
+font_title = pygame.font.Font("assets/font/Exo2-regular.ttf", 50)
+title_surf = font_title.render("My game", True, "Black")
+title_rect = title_surf.get_rect(center = (central_pos_x, 50))
+
+font_text = pygame.font.Font("assets/font/Lato-Regular.ttf", 50)
+
+play_button_surf = font_text.render("Play", True, "Black")
+play_button_rect = play_button_surf.get_rect(center = (central_pos_x, 200))
+
+editor_button_surf = font_text.render("Editor", True, "Black")
+editor_button_rect = editor_button_surf.get_rect(center = (central_pos_x, 260))
+
+tutorial_button_surf = font_text.render("Tutorial", True, "Black")
+tutorial_button_rect = tutorial_button_surf.get_rect(center = (central_pos_x, 320))
+
+options_button_surf = font_text.render("Options", True, "Black")
+options_button_rect = options_button_surf.get_rect(center = (central_pos_x, 380))
+
+credits_button_surf = font_text.render("Credits", True, "Black")
+credits_button_rect = credits_button_surf.get_rect(center = (central_pos_x, 440))
+
+exit_button_surf = font_text.render("Exit", True, "Black")
+exit_button_rect = exit_button_surf.get_rect(center = (central_pos_x, 500))
 
 
 # In-game features
@@ -46,7 +49,7 @@ while True:
             pygame.quit()
             exit()
             
-        if event_type == MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if play_button_rect.collidepoint(event.pos):
                 # Start the game
                 pass
@@ -72,16 +75,17 @@ while True:
     # Text display
     
     # Principal menu
-    central_pos_x = WIDTH / 2
     
     # Game title
-    display_text(central_pos_x, 50, "My game", "Exo2-Regular")
+    screen.blit(title_surf, title_rect)
     
     # Menu options
-    menu_options = ["Play", "Editor", "Tutorial", "Options", "Credits", "Exit"]
-    
-    for i in range(len(menu_options)):
-        display_text(central_pos_x, 250 + 80*i, menu_options[i], "Lato-Regular")
+    screen.blit(play_button_surf, play_button_rect)
+    screen.blit(editor_button_surf, editor_button_rect)
+    screen.blit(tutorial_button_surf, tutorial_button_rect)
+    screen.blit(options_button_surf, options_button_rect)
+    screen.blit(credits_button_surf, credits_button_rect)
+    screen.blit(exit_button_surf, exit_button_rect)
     
     # Update screen
     pygame.display.update()
