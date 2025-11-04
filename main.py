6fc66789796e081e009c8fe32ece7f9709c8825e
shichen556 <shichenlinxd@gmail.com>
2025-11-04 22:55:01 +0100

    feat(game): implement screen switching and remove assets folder
    Simplified the project by adding basic screen transitions
    and removing the unused assets folder for now to keep the game simple.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from os import path
 from sys import exit
     
 # Initialize pygame
@@ -15,32 +16,46 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
 # Menu buttons setup
+font_title = pygame.font.SysFont("Trebuchet MS", 80)
+font_text = pygame.font.SysFont("Tahoma", 50)
+font_button = pygame.font.SysFont("Arial Rounded MT Bold", 40)
+
 central_pos_x = WIDTH / 2
 
-font_title = pygame.font.Font("assets/font/Exo2-regular.ttf", 50)
-title_surf = font_title.render("My game", True, "Black")
-title_rect = title_surf.get_rect(center = (central_pos_x, 50))
 
-font_text = pygame.font.Font("assets/font/Lato-Regular.ttf", 50)
+title_surf = font_title.render("My game", True, "Black")
+title_rect = title_surf.get_rect(center = (central_pos_x, 80))
+
+pos_y = 250
+inter_pos_y = 80
+i=0
+in_menu = 1
 
 play_button_surf = font_text.render("Play", True, "Black")
-play_button_rect = play_button_surf.get_rect(center = (central_pos_x, 200))
+play_button_rect = play_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
 
 editor_button_surf = font_text.render("Editor", True, "Black")
-editor_button_rect = editor_button_surf.get_rect(center = (central_pos_x, 260))
+editor_button_rect = editor_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
 
 tutorial_button_surf = font_text.render("Tutorial", True, "Black")
-tutorial_button_rect = tutorial_button_surf.get_rect(center = (central_pos_x, 320))
+tutorial_button_rect = tutorial_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
 
 options_button_surf = font_text.render("Options", True, "Black")
-options_button_rect = options_button_surf.get_rect(center = (central_pos_x, 380))
+options_button_rect = options_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
 
 credits_button_surf = font_text.render("Credits", True, "Black")
-credits_button_rect = credits_button_surf.get_rect(center = (central_pos_x, 440))
+credits_button_rect = credits_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
 
 exit_button_surf = font_text.render("Exit", True, "Black")
-exit_button_rect = exit_button_surf.get_rect(center = (central_pos_x, 500))
+exit_button_rect = exit_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 
+back_button_surf = font_button.render("Back", True, "Black")
+back_button_rect = back_button_surf.get_rect(center = (50, 40))
 
 # In-game features
 while True:
@@ -52,22 +67,31 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if play_button_rect.collidepoint(event.pos):
                 # Start the game
-                pass
+                in_menu = 0
+                
+                pygame.draw.circle(screen, "yellow", (500,500), 5)
+                
             elif editor_button_rect.collidepoint(event.pos):
                 # Open the editor
-                pass
+                in_menu = 0
+                
             elif tutorial_button_rect.collidepoint(event.pos):
                 # Open the tutorial
-                pass
+                in_menu = 0
+                
             elif options_button_rect.collidepoint(event.pos):
                 # Open options menu
-                pass
+                in_menu = 0
+                
             elif credits_button_rect.collidepoint(event.pos):
                 # Show credits
-                pass
+                in_menu = 0
+                
             elif exit_button_rect.collidepoint(event.pos):
                 pygame.quit()
                 exit()
+            elif back_button_rect.collidepoint(event.pos):
+                in_menu = 1
     
     # Background color
     screen.fill((94,129,162))
@@ -76,17 +100,20 @@ while True:
     
     # Principal menu
     
-    # Game title
-    screen.blit(title_surf, title_rect)
-    
-    # Menu options
-    screen.blit(play_button_surf, play_button_rect)
-    screen.blit(editor_button_surf, editor_button_rect)
-    screen.blit(tutorial_button_surf, tutorial_button_rect)
-    screen.blit(options_button_surf, options_button_rect)
-    screen.blit(credits_button_surf, credits_button_rect)
-    screen.blit(exit_button_surf, exit_button_rect)
-    
+    if in_menu:
+        # Game title
+        screen.blit(title_surf, title_rect)
+        
+        # Menu options
+        screen.blit(play_button_surf, play_button_rect)
+        screen.blit(editor_button_surf, editor_button_rect)
+        screen.blit(tutorial_button_surf, tutorial_button_rect)
+        screen.blit(options_button_surf, options_button_rect)
+        screen.blit(credits_button_surf, credits_button_rect)
+        screen.blit(exit_button_surf, exit_button_rect)
+    else:
+        screen.blit(back_button_surf, back_button_rect)
+        
     # Update screen
     pygame.display.update()
     clock.tick(60) # Frame ceiling
