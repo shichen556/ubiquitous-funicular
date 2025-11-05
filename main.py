bad1b4d523f4ad3cdeb8242d7659b7f1645442e3
shichen556 <shichenlinxd@gmail.com>
2025-11-05 22:22:46 +0100

    feat(physics): add electron and proton objects with motion and field visualization
    Implemented electron and proton objects with basic movement.
    Added functions in main.py to draw electric and magnetic fields.
    Note: consider changing global field variables to local ones in future updates.

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import pygame
 from os import path
 from sys import exit
+
+def draw_electric_field(surface, color, spacing = 50):
+    width, height = surface.get_size()
+    arrow_length = 20
+    arrow_size = 6
     
+    for y in range(spacing//2, height, spacing):
+        # Horizontal lines
+        pygame.draw.line(surface, color, (0,y), (width, y), 2)
+        
+        # Arrow after a interval
+        for x in range(0, width, 100):
+            pygame.draw.line(surface, color,
+                             (x, y),
+                             (x + arrow_length, y - arrow_size), 2)
+            pygame.draw.line(surface, color,
+                             (x, y),
+                             (x + arrow_length, y + arrow_size), 2)
+            
+def draw_magnetic_field(surface, color_out, color_in, spacing=60, phase=0):
+    width, height = surface.get_size()
+    
+    for y in range(spacing//2, height, spacing):
+        for x in range(spacing//2, width, spacing):
+            if(x//spacing + y//spacing + phase) % 2 == 0:
+                # Campo saliente (·)
+                pygame.draw.circle(surface, color_out, (x, y), 4)
+            else:
+                # Campo entrante (x)
+                pygame.draw.line(surface, color_in, (x-4, y-4), (x+4, y+4), 2)
+                pygame.draw.line(surface, color_in, (x-4, y+4), (x+4, y-4), 2)
 # Initialize pygame
 pygame.init()
 
@@ -23,39 +53,46 @@ font_button = pygame.font.SysFont("Arial Rounded MT Bold", 40)
 central_pos_x = WIDTH / 2
 
 
-title_surf = font_title.render("My game", True, "Black")
+title_surf = font_title.render("My game", True, "#B0C4FF")
 title_rect = title_surf.get_rect(center = (central_pos_x, 80))
 
 pos_y = 250
 inter_pos_y = 80
 i=0
-in_menu = 1
+in_menu = 0
+in_play = 1
 
-play_button_surf = font_text.render("Play", True, "Black")
+pos_x = 50        
+vel = 3
+            
+button_color = ["#00BFFF", "#2A3B7A"]
+on_button = 0
+
+play_button_surf = font_text.render("Play", True, button_color[on_button])
 play_button_rect = play_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 i+=1
 
-editor_button_surf = font_text.render("Editor", True, "Black")
+editor_button_surf = font_text.render("Editor", True, button_color[on_button])
 editor_button_rect = editor_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 i+=1
 
-tutorial_button_surf = font_text.render("Tutorial", True, "Black")
+tutorial_button_surf = font_text.render("Tutorial", True, button_color[on_button])
 tutorial_button_rect = tutorial_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 i+=1
 
-options_button_surf = font_text.render("Options", True, "Black")
+options_button_surf = font_text.render("Options", True, button_color[on_button])
 options_button_rect = options_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 i+=1
 
-credits_button_surf = font_text.render("Credits", True, "Black")
+credits_button_surf = font_text.render("Credits", True, button_color[on_button])
 credits_button_rect = credits_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 i+=1
 
-exit_button_surf = font_text.render("Exit", True, "Black")
+exit_button_surf = font_text.render("Exit", True, button_color[on_button])
 exit_button_rect = exit_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
 
-back_button_surf = font_button.render("Back", True, "Black")
-back_button_rect = back_button_surf.get_rect(center = (50, 40))
+back_button_surf = font_button.render("Back to menu", True, button_color[on_button])
+back_button_rect = back_button_surf.get_rect(center = (100, 40))
 
 # In-game features
 while True:
@@ -69,7 +106,7 @@ while True:
                 # Start the game
                 in_menu = 0
                 
-                pygame.draw.circle(screen, "yellow", (500,500), 5)
+                in_play = 1
                 
             elif editor_button_rect.collidepoint(event.pos):
                 # Open the editor
@@ -92,15 +129,15 @@ while True:
                 exit()
             elif back_button_rect.collidepoint(event.pos):
                 in_menu = 1
-    
-    # Background color
-    screen.fill((94,129,162))
+                in_play = 0
     
     # Text display
     
     # Principal menu
     
     if in_menu:
+        # Background color
+        screen.fill("#0A0A23")
         # Game title
         screen.blit(title_surf, title_rect)
         
@@ -111,9 +148,33 @@ while True:
         screen.blit(options_button_surf, options_button_rect)
         screen.blit(credits_button_surf, credits_button_rect)
         screen.blit(exit_button_surf, exit_button_rect)
-    else:
-        screen.blit(back_button_surf, back_button_rect)
+    if not in_menu:
+        screen.fill("#0A0A23")
         
+        screen.blit(back_button_surf, back_button_rect)
+        if in_play:
+            pos_x += vel
+            
+            if pos_x >= 1000:
+                pos_x = -100
+                
+            # Negative charge
+            electron = pygame.draw.circle(screen, "#1E90FF", (pos_x, 200), 10)
+            
+            # Positive charge
+            proton = pygame.draw.circle(screen, "#FF4500", (pos_x,300), 10)
+            
+            E_field_color = "#FFD700"
+            
+            #draw_electric_field(screen, E_field_color)
+            
+            B_out_color = "#00FFCC"
+            B_in_color = "#9B30FF"
+            
+            draw_magnetic_field(screen, B_out_color, B_in_color)
+            
+            
+            
     # Update screen
     pygame.display.update()
     clock.tick(60) # Frame ceiling
