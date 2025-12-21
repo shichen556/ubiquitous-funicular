d96f1e947aff73aed0b5808076c83e4994a8256e
shichen556 <shichenlinxd@gmail.com>
2025-12-21 20:09:35 +0100

    Implementacion de interacion con campo B frente con carga positiva. Nota Nota: Falta refactorizar y cambiarlo a POO

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 import pygame
 from os import path
 from sys import exit
+from math import sin, cos
 
-def draw_electric_field(rect, color, spacing = 50):
+# class Options:
+#     def __init__(self, font, text):
+#         self.font=font
+#         self.text=text
+#         self.central_pos_x=WIDTH / 2
+        
+# class Title(Options):
+#     def __init__(self, font, text):
+#         super().__init__(font, text, central_pos_x)
+    
+# class Button(Options):
+#     def __init__(self, font, text):
+#         super().__init__(font, text, central_pos_x)
+
+# class Menu:
+#     def __init__(self):
+#         self.opciones = []
+        
+#     def display_menu():
+#         pass
+        
+
+# class Particle:
+#     def __init__(self, pos_x, pos_y, charge):
+#         self.pos_x = pos_x
+#         self.pos_y = pos_y
+#         self.charge = charge
+    
+#     def draw_particle(color):
+#         pygame.draw.circle(screen, color, (pos_x, pos_y), 10)
+
+
+def draw_electric_field(rect, spacing = 50):
+    color = "#FFD700"
     area_subsurf = screen.subsurface(rect)
     pygame.draw.rect(screen, (255,255,255), rect, 1)
     
@@ -24,7 +58,8 @@ def draw_electric_field(rect, color, spacing = 50):
                              (x, y),
                              (x + arrow_length, y + arrow_size), 2)
             
-def draw_magnetic_field_out(rect, color_out, spacing=60):
+def draw_magnetic_field_out(rect, spacing=60):
+    color = "#00FFCC"
     area_subsurf = screen.subsurface(rect)
     pygame.draw.rect(screen, (255,255,255), rect, 1)
     
@@ -33,21 +68,29 @@ def draw_magnetic_field_out(rect, color_out, spacing=60):
     for y in range(spacing//3, height, spacing):
         for x in range(spacing//3, width, spacing):
             # Campo saliente (·)
-            pygame.draw.circle(area_subsurf, color_out, (x, y), 12, 1)
-            pygame.draw.circle(area_subsurf, color_out, (x, y), 4)
+            pygame.draw.circle(area_subsurf, color, (x, y), 12, 1)
+            pygame.draw.circle(area_subsurf, color, (x, y), 4)
                 
-def draw_magnetic_field_in(rect, color_in, spacing=60):
-    area_subsurf = screen.subsurface(rect)
+def draw_magnetic_field_in(rect, spacing=60):
+    color =  "#9B30FF"
     pygame.draw.rect(screen, (255,255,255), rect, 1)
     
-    width, height = area_subsurf.get_size()
-    for y in range(spacing//3, height, spacing):
-        for x in range(spacing//3, width, spacing):
+    for y in range(rect.top + 20, rect.bottom, spacing):
+        for x in range(rect.left + 20, rect.right, spacing):
             # Campo entrante (x)
-            pygame.draw.circle(area_subsurf, color_in, (x, y), 12, 1)
-            pygame.draw.line(area_subsurf, color_in, (x-4, y-4), (x+4, y+4), 2)
-            pygame.draw.line(area_subsurf, color_in, (x-4, y+4), (x+4, y-4), 2)
+            pygame.draw.circle(screen, color, (x, y), 12, 1)
+            pygame.draw.line(screen, color, (x-4, y-4), (x+4, y+4), 2)
+            pygame.draw.line(screen, color, (x-4, y+4), (x+4, y-4), 2)
 
+def rotate(x, y):
+    angulo += vel_ang
+    
+    v_x = vel * cos(angulo)
+    v_y = vel * sin(angulo)
+    
+    x += v_x
+    y += v_y
+    
 # Initialize pygame
 pygame.init()
 
@@ -68,7 +111,6 @@ font_button = pygame.font.SysFont("Arial Rounded MT Bold", 40)
 
 central_pos_x = WIDTH / 2
 
-
 title_surf = font_title.render("My game", True, "#B0C4FF")
 title_rect = title_surf.get_rect(center = (central_pos_x, 80))
 
@@ -78,8 +120,7 @@ i=0
 in_menu = 0
 in_play = 1
 
-pos_x = 50        
-vel = 3
+pos_x = 50
             
 button_color = ["#00BFFF", "#2A3B7A"]
 on_button = 0
@@ -112,7 +153,28 @@ back_button_rect = back_button_surf.get_rect(center = (100, 40))
 
 area_pos_x = 450
 area_pos_y = 350
+
+vel = 5
+v_x = vel
+v_y = 0
+
+e_pos_x=50
+e_pos_y=200
+
             
+# p_pos_x=200
+# p_pos_y=300
+
+# Constantes
+q = 1.602
+m = 9.11
+B = 1
+
+# valores MCU
+radio = m*vel/(q*B)
+angulo = 0
+vel_ang = vel/radio
+
 # In-game features
 while True:
     for event in pygame.event.get():
@@ -173,30 +235,45 @@ while True:
         
         screen.blit(back_button_surf, back_button_rect)
         if in_play:
-            pos_x += vel
-            
-            if pos_x >= 1000:
-                pos_x = -100
-            
-            E_field_color = "#FFD700"
-            B_out_color = "#00FFCC"
-            B_in_color = "#9B30FF"
+            # electron = Particle(e_pos_x, e_pos_y, "Negative")
+            # proton = Particle(p_pos_x, p_pos_y, "Positive")
             
             area_rect = pygame.Rect(area_pos_x - 50, area_pos_y - 50, 100, 100)
+            draw_magnetic_field_in(area_rect)
+            
+            # Negative charge
+            # electron.draw_particle(color = "#1E90FF")
+            electron = pygame.draw.circle(screen, "#1E90FF", (e_pos_x, e_pos_y), 10)
+            
+            # Positive charge
+            # proton = pygame.draw.circle(screen, "#FF4500", (pos_x,300), 10)
+            
+            if e_pos_x > 1000 or e_pos_x < -100 or e_pos_y > 800 or e_pos_y < -100:
+                e_pos_x = -100
+                e_pos_y = 200
+                
+                angulo = 0
+                v_x = vel
+                v_y = 0
+            
+            if electron.colliderect(area_rect):
+                # MCU
+                angulo -= vel_ang
+    
+                v_x = vel * cos(angulo)
+                v_y = vel * sin(angulo)
+                
+                e_pos_x += v_x
+                e_pos_y += v_y
+            else:
+                # MRU
+                e_pos_x += v_x
+                e_pos_y += v_y
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 area_pos_x, area_pos_y = pygame.mouse.get_pos()
                 print(area_pos_x, area_pos_y)
                  
-            draw_magnetic_field_in(area_rect, B_in_color)
-            
-            # Negative charge
-            electron = pygame.draw.circle(screen, "#1E90FF", (pos_x, 200), 10)
-            
-            # Positive charge
-            proton = pygame.draw.circle(screen, "#FF4500", (pos_x,300), 10)
-            
-            
     # Update screen
     pygame.display.update()
     clock.tick(60) # Frame ceiling
