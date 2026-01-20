5a7b573ac4c99a3669365a7cd92bf1c95608ade5
shichen556 <shichenlinxd@gmail.com>
2026-01-20 14:14:53 +0100

    add(): holder/main.py and refactor.py

---
+++ Holder/main.py
@@ -0,0 +1,281 @@
+import pygame
+from sys import exit
+from math import sin, cos
+
+# class Options:
+#     def __init__(self, font, text):
+#         self.font=font
+#         self.text=text
+#         self.central_pos_x=WIDTH / 2
+        
+# class Title(Options):
+#     def __init__(self, font, text):
+#         super().__init__(font, text, central_pos_x)
+    
+# class Button(Options):
+#     def __init__(self, font, text):
+#         super().__init__(font, text, central_pos_x)
+
+# class Menu:
+#     def __init__(self):
+#         self.title=Title()
+#         self.button=Button()
+#         self.opciones = []
+        
+#     def display_menu():
+#         pass
+        
+
+# class Particle:
+#     def __init__(self, pos_x, pos_y, charge):
+#         self.pos_x = pos_x
+#         self.pos_y = pos_y
+#         self.charge = charge
+    
+#     def draw_particle(color):
+#         pygame.draw.circle(screen, color, (pos_x, pos_y), 10)
+
+
+def draw_electric_field(rect, spacing = 50):
+    color = "#FFD700"
+    area_subsurf = screen.subsurface(rect)
+    pygame.draw.rect(screen, (255,255,255), rect, 1)
+    
+    width, height = area_subsurf.get_size()
+    
+    arrow_length = 20
+    arrow_size = 6
+    
+    for y in range(spacing//2, height, spacing):
+        # Horizontal lines
+        pygame.draw.line(area_subsurf, color, (0,y), (width, y), 2)
+        
+        # Arrow after a interval
+        for x in range(0, width, 100):
+            pygame.draw.line(area_subsurf, color,
+                             (x, y),
+                             (x + arrow_length, y - arrow_size), 2)
+            pygame.draw.line(area_subsurf, color,
+                             (x, y),
+                             (x + arrow_length, y + arrow_size), 2)
+            
+def draw_magnetic_field_out(rect, spacing=60):
+    color = "#00FFCC"
+    area_subsurf = screen.subsurface(rect)
+    pygame.draw.rect(screen, (255,255,255), rect, 1)
+    
+    width, height = area_subsurf.get_size()
+    
+    for y in range(spacing//3, height, spacing):
+        for x in range(spacing//3, width, spacing):
+            # Campo saliente (·)
+            pygame.draw.circle(area_subsurf, color, (x, y), 12, 1)
+            pygame.draw.circle(area_subsurf, color, (x, y), 4)
+                
+def draw_magnetic_field_in(rect, spacing=60):
+    color =  "#9B30FF"
+    pygame.draw.rect(screen, (255,255,255), rect, 1)
+    
+    for y in range(rect.top + 20, rect.bottom, spacing):
+        for x in range(rect.left + 20, rect.right, spacing):
+            # Campo entrante (x)
+            pygame.draw.circle(screen, color, (x, y), 12, 1)
+            pygame.draw.line(screen, color, (x-4, y-4), (x+4, y+4), 2)
+            pygame.draw.line(screen, color, (x-4, y+4), (x+4, y-4), 2)
+
+def rotate(x, y):
+    angulo += vel_ang
+    
+    v_x = vel * cos(angulo)
+    v_y = vel * sin(angulo)
+    
+    x += v_x
+    y += v_y
+    
+# Initialize pygame
+pygame.init()
+
+# Set up pygame screen
+WIDTH = 900
+HEIGHT = 700
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Runner")
+
+# Framerate control
+clock = pygame.time.Clock()
+
+# Menu buttons setup
+font_title = pygame.font.SysFont("Trebuchet MS", 80)
+font_text = pygame.font.SysFont("Tahoma", 50)
+font_button = pygame.font.SysFont("Arial Rounded MT Bold", 40)
+
+central_pos_x = WIDTH / 2
+
+title_surf = font_title.render("My game", True, "#B0C4FF")
+title_rect = title_surf.get_rect(center = (central_pos_x, 80))
+
+pos_y = 250
+inter_pos_y = 80
+i=0
+in_menu = 0
+in_play = 1
+
+pos_x = 50
+            
+button_color = ["#00BFFF", "#2A3B7A"]
+on_button = 0
+
+play_button_surf = font_text.render("Play", True, button_color[on_button])
+play_button_rect = play_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
+
+editor_button_surf = font_text.render("Editor", True, button_color[on_button])
+editor_button_rect = editor_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
+
+tutorial_button_surf = font_text.render("Tutorial", True, button_color[on_button])
+tutorial_button_rect = tutorial_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
+
+options_button_surf = font_text.render("Options", True, button_color[on_button])
+options_button_rect = options_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
+
+credits_button_surf = font_text.render("Credits", True, button_color[on_button])
+credits_button_rect = credits_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+i+=1
+
+exit_button_surf = font_text.render("Exit", True, button_color[on_button])
+exit_button_rect = exit_button_surf.get_rect(center = (central_pos_x, pos_y+inter_pos_y*i))
+
+back_button_surf = font_button.render("Back to menu", True, button_color[on_button])
+back_button_rect = back_button_surf.get_rect(center = (100, 40))
+
+area_pos_x = 450
+area_pos_y = 350
+
+vel = 5
+v_x = vel
+v_y = 0
+
+e_pos_x=50
+e_pos_y=200
+
+            
+# p_pos_x=200
+# p_pos_y=300
+
+# Constantes
+q = 1.602
+m = 9.11
+B = 1
+
+# valores MCU
+radio = m*vel/(q*B)
+angulo = 0
+vel_ang = vel/radio
+
+# In-game features
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if play_button_rect.collidepoint(event.pos):
+                # Start the game
+                in_menu = 0
+                
+                in_play = 1
+                
+            elif editor_button_rect.collidepoint(event.pos):
+                # Open the editor
+                in_menu = 0
+                
+            elif tutorial_button_rect.collidepoint(event.pos):
+                # Open the tutorial
+                in_menu = 0
+                
+            elif options_button_rect.collidepoint(event.pos):
+                # Open options menu
+                in_menu = 0
+                
+            elif credits_button_rect.collidepoint(event.pos):
+                # Show credits
+                in_menu = 0
+                
+            elif exit_button_rect.collidepoint(event.pos):
+                pygame.quit()
+                exit()
+            elif back_button_rect.collidepoint(event.pos):
+                in_menu = 1
+                in_play = 0
+    
+    # Text display
+    
+    # Principal menu
+    
+    if in_menu:
+        # Background color
+        screen.fill("#0A0A23")
+        # Game title
+        screen.blit(title_surf, title_rect)
+        
+        # Menu options
+        screen.blit(play_button_surf, play_button_rect)
+        screen.blit(editor_button_surf, editor_button_rect)
+        screen.blit(tutorial_button_surf, tutorial_button_rect)
+        screen.blit(options_button_surf, options_button_rect)
+        screen.blit(credits_button_surf, credits_button_rect)
+        screen.blit(exit_button_surf, exit_button_rect)
+        
+    if not in_menu:
+        screen.fill("#0A0A23")
+        
+        screen.blit(back_button_surf, back_button_rect)
+        if in_play:
+            # electron = Particle(e_pos_x, e_pos_y, "Negative")
+            # proton = Particle(p_pos_x, p_pos_y, "Positive")
+            
+            area_rect = pygame.Rect(area_pos_x - 50, area_pos_y - 50, 100, 100)
+            draw_magnetic_field_in(area_rect)
+            
+            # Negative charge
+            # electron.draw_particle(color = "#1E90FF")
+            electron = pygame.draw.circle(screen, "#1E90FF", (e_pos_x, e_pos_y), 10)
+            
+            # Positive charge
+            # proton = pygame.draw.circle(screen, "#FF4500", (pos_x,300), 10)
+            
+            if e_pos_x > 1000 or e_pos_x < -100 or e_pos_y > 800 or e_pos_y < -100:
+                e_pos_x = -100
+                e_pos_y = 200
+                
+                angulo = 0
+                v_x = vel
+                v_y = 0
+            
+            if electron.colliderect(area_rect):
+                # MCU
+                angulo -= vel_ang
+    
+                v_x = vel * cos(angulo)
+                v_y = vel * sin(angulo)
+                
+                e_pos_x += v_x
+                e_pos_y += v_y
+            else:
+                # MRU
+                e_pos_x += v_x
+                e_pos_y += v_y
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                area_pos_x, area_pos_y = pygame.mouse.get_pos()
+                print(area_pos_x, area_pos_y)
+                 
+    # Update screen
+    pygame.display.update()
+    clock.tick(60) # Frame ceiling
+    
---
+++ Holder/refactor.py
@@ -0,0 +1,311 @@
+from math import sin, cos
+import pygame
+
+class Scene:
+    def handle_events(self, events):
+        pass
+    
+    def update(self, dt):
+        pass
+    
+    def draw(self, screen):
+        pass
+
+class Game:
+    def __init__(self):
+        # Initialize pygame
+        pygame.init()
+        
+        # Set up pygame screen
+        self.WIDTH = 900
+        self.HEIGHT = 700
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Runner")
+        
+        # Framerate control
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.current_scene=None
+    
+    def change_scene(self, scene):
+        self.current_scene = scene
+    
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(60) / 1000 # Frame ceiling
+            
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            self.current_scene.handle_events(events)
+            self.current_scene.update(dt)
+            self.current_scene.draw(self.screen)
+            
+            pygame.display.update()
+        
+        pygame.quit()
+
+# Game states
+class MenuScene(Scene):
+    def __init__(self, game):
+        self.game = game
+        self.font = pygame.font.SysFont("Trebuchet MS", 80)
+        
+        self.buttons = [
+            Button(350, 200, 200, 60, "Play", self.start_game),
+            Button(350, 300, 200, 60, "Options", self.open_options),
+            Button(350, 400, 200, 60, "Exit", self.quit_game)
+        ]
+    
+    def start_game(self):
+        print("Starting the game...")
+        self.game.change_scene(PlayScene(self.game))
+    
+    def open_options(self):
+        print("Opening options...")
+        self.game.change_scene(OptionsScene(self.game))
+    
+    def quit_game(self):
+        self.game.running = False
+    
+    def handle_events(self, events):
+        for button in self.buttons:
+            button.handle_events(events)
+    
+    def update(self, dt):
+        pass
+    
+    def draw(self, screen):
+        # Background color
+        screen.fill("#0A0A23")
+        title_surf = self.font.render("My Game", True, "#B0C4FF")
+        title_rect = title_surf.get_rect(center = (self.game.WIDTH // 2, 80))
+        
+        # Game title
+        screen.blit(title_surf, title_rect)
+        
+        for button in self.buttons:
+            button.draw(screen)
+        
+class PlayScene(Scene):
+    def __init__(self, game):
+        self.game = game
+        self.font = pygame.font.SysFont("Arial", 30)
+        
+        self.electron = Particle(50, 200, "Negative")
+        self.proton = Particle(200, 300, "Positive")
+        
+        self.electric_field = ElectricField(pygame.Rect(450 - 50, 350 - 50, 100, 100))
+        self.magnetic_field = MagneticField(
+            pygame.Rect(450 - 50, 350 - 50, 100, 100), 
+            field_type="in")
+        
+        self.fields = [self.magnetic_field]
+        
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.game.change_scene(MenuScene(self.game))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Permitir cambiar la pos del campo con el clic del raton
+                self.electric_field.rect.center=pygame.mouse.get_pos()
+                self.magnetic_field.rect.center=pygame.mouse.get_pos()
+    
+    def update(self, dt):
+        for field in self.fields:
+            if field.contains(self.electron.pos_x, self.electron.pos_y):
+                field.apply_to(self.electron, dt)
+        self.electron.update(dt)
+        # self.proton.update(dt)
+    
+    def draw(self, screen):
+        # Background color
+        screen.fill("#0A0A23")
+        
+        # self.electric_field.draw(screen)
+        self.magnetic_field.draw(screen)
+        
+        self.electron.draw(screen)
+        # self.proton.draw(screen)
+        
+        back_button = Button(50,50,200,50, "Back to Menu", self.go_back)
+        back_button.draw(screen)
+
+    def go_back(self):
+        self.game.change_scene(MenuScene(self.game))
+        
+class PauseScene(Scene):
+    def __init__(self, game):
+        self.game = game
+
+    def handle_events(self, events):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pass
+
+
+class OptionsScene(Scene):
+    def __init__(self, game):
+        self.game = game
+
+    def handle_events(self, events):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        pass
+
+class Button:
+    def __init__(self, x, y, width, height, text, action):
+        self.rect = pygame.Rect(x,y,width, height)
+        self.text = text
+        self.action = action
+        self.font = pygame.font.SysFont("Tahoma", 50)
+        self.color = "#00BFFF"
+        self.hover_color = "#2A3B7A"
+        self.is_hovered = False
+
+    def draw(self, screen):
+        if self.is_hovered:
+            color = self.hover_color
+        else:
+            color = self.color
+        
+        pygame.draw.rect(screen, color, self.rect)
+        text_surf = self.font.render(self.text, True, "#FFFFFF")
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        screen.blit(text_surf, text_rect)
+    
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.rect.collidepoint(event.pos):
+                    self.action() # Ejecutar la accion asociada al boton
+            elif event.type == pygame.MOUSEMOTION:
+                self.is_hovered = self.rect.collidepoint(event.pos)
+        
+class Entity:
+    pass
+
+class Particle:
+    def __init__(self, pos_x, pos_y, charge, mass = 9.11, q=1.602):
+        self.pos_x=pos_x
+        self.pos_y=pos_y
+        self.charge=charge
+        self.mass=mass
+        self.q=q
+        self.vel = 5
+        self.v_x = self.vel
+        self.v_y = 0
+        self.angulo = 0
+        if charge == "Negative":
+            self.color = "#1E90FF"
+        else:
+            self.color = "#FF4500"
+    
+    def update(self, dt):
+        if self.pos_x > 1000 or self.pos_x < -100 or self.pos_y > 800 or self.pos_y < -100:
+            self.reset_pos()
+            
+        # if self.is_in_field():
+        #     # MCU
+        #     self.v_x = self.vel * cos(self.angulo)
+        #     self.v_y = self.vel * sin(self.angulo)
+        
+        # MRU
+        self.pos_x += self.v_x
+        self.pos_y += self.v_y
+    
+    def reset_pos(self):
+        self.pos_x = -100
+        self.pos_y = 200
+        self.v_x = self.vel
+        self.v_y = 0
+        self.angulo = 0
+    
+    def is_in_field(self):
+        return False
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.pos_x, self.pos_y), 10)
+
+class Field:
+    def contains(self,x,y):
+        pass
+    
+    def apply_to(self, particle, dt):
+        pass
+    
+class ElectricField(Field):
+    def __init__(self, rect, spacing=50):
+        self.rect=rect
+        self.spacing=spacing
+        self.color="#FFD700"
+    
+    def draw(self, screen):
+        area_subsurf = screen.subsurface(self.rect)
+        pygame.draw.rect(screen, (255,255,255), self.rect, 1)
+        
+        width, height = area_subsurf.get_size()
+    
+        arrow_length = 20
+        arrow_size = 6
+
+        for y in range(self.spacing//2, height, self.spacing):
+            # Horizontal lines
+            pygame.draw.line(area_subsurf, self.color, (0,y), (width, y), 2)
+            
+            # Arrow after a interval
+            for x in range(0, width, 100):
+                pygame.draw.line(area_subsurf, self.color,
+                                (x, y),
+                                (x + arrow_length, y - arrow_size), 2)
+                pygame.draw.line(area_subsurf, self.color,
+                                (x, y),
+                                (x + arrow_length, y + arrow_size), 2)
+
+class MagneticField(Field):
+    def __init__(self, rect, field_type="out", spacing=60):
+        self.rect=rect
+        self.field_type=field_type
+        self.spacing=spacing
+        if field_type == "out":
+            self.color = "#00FFCC"
+        else:
+            self.color = "#9B30FF"
+    
+    def contains(self, x, y):
+        return self.rect.collidepoint(x,y)
+    
+    def apply_to(self, particle, dt):
+        particle.angulo -= 2 * dt
+        particle.v_x = particle.vel * cos(particle.angulo)
+        particle.v_y = particle.vel * sin(particle.angulo)
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255,255,255), self.rect, 1)
+        
+        for y in range(self.rect.top + 20, self.rect.bottom, self.spacing):
+            for x in range(self.rect.left + 20, self.rect.right, self.spacing):
+                if self.field_type=="out":
+                    # Campo saliente (·)
+                    pygame.draw.circle(screen, self.color, (x, y), 12, 1)
+                    pygame.draw.circle(screen, self.color, (x, y), 4)
+                elif self.field_type=="in":
+                    # Campo entrante (x)
+                    pygame.draw.circle(screen, self.color, (x, y), 12, 1)
+                    pygame.draw.line(screen, self.color, (x-4, y-4), (x+4, y+4), 2)
+                    pygame.draw.line(screen, self.color, (x-4, y+4), (x+4, y-4), 2)
+
+game = Game()
+game.change_scene(MenuScene(game))
+game.run()
